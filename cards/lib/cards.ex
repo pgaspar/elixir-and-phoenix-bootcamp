18e98731f6c18bb46a8f8449f78867785c3b65bc
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-08-05 03:34:57 +0100

    Initial create_deck function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,6 +3,25 @@ defmodule Cards do
   Documentation for Cards.
   """
 
+  @deck_values ["Ace", "King", "Jack", "Queen", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+  @deck_suits ["Spades", "Clubs", "Hearts", "Diamonds"]
+
+  @doc """
+  Generates a deck of cards.
+  """
+  def create_deck do
+    cards = for suit <- @deck_suits do
+      for value <- @deck_values do
+        "#{value} of #{suit}"
+      end
+    end
+
+    List.flatten(cards)
+  end
+
+  @doc """
+  Shuffles the deck.
+  """
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,6 +2,12 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
+  test "creates a deck" do
+    deck = Cards.create_deck
+    assert Enum.member?(deck, "Ace of Spades") == true
+    assert Enum.member?(deck, "Two of Diamonds") == true
+  end
+
   test "shuffles a deck" do
     :rand.seed(:exsplus, {1, 2, 3})
     deck = ["Ace", "Two", "Three"]
