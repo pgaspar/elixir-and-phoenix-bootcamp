8c06d107d83dc11675dbbddaec0ef74d6fb64cdb
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-08-05 03:10:19 +0100

    Remove hello function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -3,19 +3,6 @@ defmodule Cards do
   Documentation for Cards.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
-
   def shuffle(deck) do
     Enum.shuffle(deck)
   end
--- cards/test/cards_test.exs
+++ cards/test/cards_test.exs
@@ -2,10 +2,6 @@ defmodule CardsTest do
   use ExUnit.Case
   doctest Cards
 
-  test "greets the world" do
-    assert Cards.hello() == :world
-  end
-
   test "shuffles a deck" do
     :rand.seed(:exsplus, {1, 2, 3})
     deck = ["Ace", "Two", "Three"]
