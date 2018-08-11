bd05169866c375ba8eb0ca46bd98d6eb9a2263bd
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-08-11 03:13:31 +0100

    Condensed load function

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -63,11 +63,9 @@ defmodule Cards do
   Given a filename, loads a deck from the file.
   """
   def load(filename) do
-    { status, binary } = File.read(filename)
-
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "File does not exist"
+    case File.read(filename) do
+      { :ok, binary } -> :erlang.binary_to_term(binary)
+      { :error, _ } -> "File does not exist"
     end
   end
 end
--- notes/section_2.md
+++ notes/section_2.md
@@ -5,10 +5,8 @@ It's Elixir's replacement to variable assignment.
 # Error handling
 
 ```
-{ status, binary } = File.read(filename)
-
-case status do
-  :ok -> :erlang.binary_to_term(binary)
-  :error -> "File does not exist"
+case File.read(filename) do
+  { :ok, binary } -> :erlang.binary_to_term(binary)
+  { :error, _ } -> "File does not exist"
 end
 ```
