09f35bfa3706acda3810673a7fb09670df0eb342
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-08-11 23:56:19 +0100

    Add hash_input function

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -3,16 +3,22 @@ defmodule Identicon do
   Documentation for Identicon.
   """
 
+  def main(input) do
+    input
+    |> hash_input
+  end
+
   @doc """
-  Hello world.
+  Encodes an input string using MD5, return a
+  list of numbers unique to the input.
 
-  ## Examples
+  ## Examples:
 
-      iex> Identicon.hello()
-      :world
-
+      iex> Identicon.hash_input("banana")
+      [114, 179, 2, 191, 41, 122, 34, 138, 117, 115, 1, 35, 239, 239, 124, 65]
   """
-  def hello do
-    :world
+  def hash_input(input) do
+    :crypto.hash(:md5, input)
+    |> :binary.bin_to_list
   end
 end
