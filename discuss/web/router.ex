84173ed9f297057ea86e950f146ef97a47afe981
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-08-22 21:57:51 +0100

    Use resources router helper

--- discuss/web/router.ex
+++ discuss/web/router.ex
@@ -17,10 +17,7 @@ defmodule Discuss.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", TopicController, :index
-    get "/topics/new", TopicController, :new
-    post "/topics", TopicController, :create
-    get "/topics/:id/edit", TopicController, :edit
-    put "/topics/:id", TopicController, :update
+    resources "/topics", TopicController, except: [:index, :delete]
   end
 
   # Other scopes may use custom stacks.
