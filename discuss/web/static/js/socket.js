86ce1025f643af597a67f030fd3102d09072efc6
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-09-11 02:48:43 +0100

    Start refactoring JS code

--- discuss/web/static/js/socket.js
+++ discuss/web/static/js/socket.js
@@ -53,14 +53,11 @@ let socket = new Socket("/socket", {params: {token: window.userToken}})
 
 socket.connect()
 
-// Now that you are connected, you can join channels with a topic:
-let channel = socket.channel("comments:1", {})
-channel.join()
-  .receive("ok", resp => { console.log("Joined successfully", resp) })
-  .receive("error", resp => { console.log("Unable to join", resp) })
+const createSocket = (topicId) => {
+  let channel = socket.channel(`comments:${topicId}`, {})
+  channel.join()
+    .receive("ok", resp => { console.log("Joined successfully", resp) })
+    .receive("error", resp => { console.log("Unable to join", resp) })
+}
 
-document.querySelector('button').addEventListener('click', function() {
-  channel.push('comment:hello', { hi: "there" });
-});
-
-export default socket
+window.createSocket = createSocket;
