a44d4bfc3fe3ac4b16556fa5fe419e7e49e6873e
Pedro Gaspar <pedro.gaxpar@gmail.com>
2018-09-11 03:12:51 +0100

    Fix to Comment model allowing timestamp creation

--- discuss/web/models/comment.ex
+++ discuss/web/models/comment.ex
@@ -5,6 +5,8 @@ defmodule Discuss.Comment do
     field :content, :string
     belongs_to :user, Discuss.User, on_replace: :update
     belongs_to :topic, Discuss.Topic, on_replace: :update
+
+    timestamps()
   end
 
   def changeset(struct, params \\ %{}) do
